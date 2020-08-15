27ffe38034975c194feb4ccddf0fd85c4b25bf3c
swordpower <swordpower@pop-os.localdomain>
2020-08-15 07:56:39 -0300

    Verificando erro502

--- blog/settings.py
+++ blog/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = "lv)-=^8fub_yuojc^%dhl^706@wk0yakf(6m6u_d6p5ie1xs3b"
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ["blog.swordpower.tk"]
 
