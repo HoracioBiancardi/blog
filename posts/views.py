65620f1ffcb1170be0224be7ec5eba6b6e8dcd3a
swordpower <swordpower@pop-os.localdomain>
2020-08-29 23:30:47 -0300

    Melhorando o SEO titulo categoria

--- posts/views.py
+++ posts/views.py
@@ -55,6 +55,10 @@ class PostBusca(PostIndex):
 
 class PostCategoria(PostIndex):
     template_name = "posts/post_categoria.html"
+    def get_context_data(self, **kwargs):
+        context = super(PostIndex, self).get_context_data(**kwargs)
+        context["categorias"] = Categoria.objects.all()
+        return context
 
     def get_queryset(self):
         qs = super().get_queryset()
